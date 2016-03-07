41507fc0f507e93f353feb7efcdb7e04d5dffc4e
Mike Wickwar <mike.wickwar@gmail.com>
2016-03-06 23:58:36 -0700

    deploying

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
+var knex = require('../db/knex.js');
+var Skills = function () {
+  return knex('skills')
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'PORTFOLIO' });
+});
+
+router.get('/skills', function(req, res, next) {
+  Skills().select().then(function (skills) {
+    res.json(skills)
+  })
 });
 
 module.exports = router;
