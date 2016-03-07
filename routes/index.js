163e6b68edcddb34c420e3cc046734878ff681d7
Mike Wickwar <mike.wickwar@gmail.com>
2016-03-07 00:30:11 -0700

    route added for ed

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ var knex = require('../db/knex.js');
 var Skills = function () {
   return knex('skills')
 }
+var Ed = function () {
+  return knex('ed')
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'PORTFOLIO' });
@@ -12,6 +15,12 @@ router.get('/', function(req, res, next) {
 router.get('/skills', function(req, res, next) {
   Skills().select().then(function (skills) {
     res.json(skills)
+  })
+});
+
+router.get('/ed', function(req, res, next) {
+  Ed().select().then(function (ed) {
+    res.json(ed)
   })
 });
 
