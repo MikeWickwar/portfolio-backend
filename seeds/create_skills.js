7da6916e25df9de1ee08dccc08949a7069ed727e
Mike Wickwar <mike.wickwar@gmail.com>
2016-03-09 15:12:03 -0700

    fixing cors issues

--- seeds/create_skills.js
+++ seeds/create_skills.js
@@ -1,28 +1,26 @@
 
 exports.seed = function(knex, Promise) {
   return Promise.join(
-    // Deletes ALL existing entries
     knex('skills').del(),
-
-    // Inserts seed entries
-    knex('skills').insert({id: 1, skill: 'Node.js'}),
-    knex('skills').insert({id: 2, skill: 'HTML'}),
-    knex('skills').insert({id: 3, skill: 'CSS'}),
-    knex('skills').insert({id: 4, skill: 'Postgres'}),
-    knex('skills').insert({id: 5, skill: 'Express'}),
-    knex('skills').insert({id: 6, skill: 'Angular'}),
-    knex('skills').insert({id: 7, skill: 'Bootstrap'}),
-    knex('skills').insert({id: 8, skill: 'OAuth'}),
-    knex('skills').insert({id: 9, skill: 'jquery'}),
-    knex('skills').insert({id: 10, skill: 'Jade'}),
-    knex('skills').insert({id: 11, skill: 'Git'}),
-    knex('skills').insert({id: 12, skill: 'Pivitol Tracker'}),
-    knex('skills').insert({id: 13, skill: 'Mocha/Chai'}),
+    knex('skills').insert({id: 1, skill: 'badges/nodejs.png'}),
+    knex('skills').insert({id: 4, skill: 'badges/pg-commander-icon.png'}),
+    knex('skills').insert({id: 5, skill: 'badges/expressjs.png'}),
+    knex('skills').insert({id: 6, skill: 'badges/angular-js-icon.jpg'}),
+    knex('skills').insert({id: 2, skill: 'badges/HTML5_Logo_256.png'}),
+    knex('skills').insert({id: 3, skill: 'badges/icon_css3.png'}),
+    knex('skills').insert({id: 7, skill: 'badges/bootstrapicon.jpeg'}),
+    knex('skills').insert({id: 8, skill: 'badges/OAuth-Shine-300x298.png'}),
+    knex('skills').insert({id: 9, skill: 'badges/download.png'}),
+    knex('skills').insert({id: 10, skill: 'badges/jadejsicon.png'}),
+    knex('skills').insert({id: 11, skill: 'badges/octocat.png'}),
+    knex('skills').insert({id: 12, skill: 'badges/pivotaltrackicon.png'}),
+    knex('skills').insert({id: 13, skill: 'badges/chaijs.png'}),
     knex('skills').insert({id: 14, skill: 'Database Modeling'}),
     knex('skills').insert({id: 15, skill: 'Responsive Design'}),
     knex('skills').insert({id: 16, skill: 'Paired Programming'}),
     knex('skills').insert({id: 17, skill: 'Group Programming'}),
     knex('skills').insert({id: 18, skill: 'Leadership'}),
-    knex('skills').insert({id: 19, skill: 'Teaching'})
+    knex('skills').insert({id: 19, skill: 'Teaching'}),
+    knex('skills').insert({id: 20, skill: 'badges/mocha.png'})
   );
 };
